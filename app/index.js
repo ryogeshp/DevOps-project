a88f0814bad0bb46098ca403d7a7ab9c07a38e3f
ryogeshp <rjprajapati08@gmail.com>
2025-07-17 15:21:56 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -32,7 +32,7 @@ function sendCalculatorUI(res, currentExpression = '', result = '', isError = fa
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Professional Calculator</title>
+      <title> Professional Calculator </title>
       <style>
         * {
           box-sizing: border-box;
